23cf0f46e8746cd18ec103543ce0cf4b1d226640
segev <segev@Ubunto.myguest.virtualbox.org>
2023-03-18 09:59:28 +0200

    some fix and commands

--- AdptArray.c
+++ AdptArray.c
@@ -5,26 +5,26 @@
 #define init 1
 
 struct AdptArray_ {
-    int size;
-    int cap;
-    PElement *data;
+    int size; // the size of the arry
+    PElement *data; //data
+    //the build funcs:
     COPY_FUNC copyFunc;
     DEL_FUNC delFunc;
     PRINT_FUNC printFunc;
 };
 
 PAdptArray CreateAdptArray(COPY_FUNC copyFunc, DEL_FUNC delFunc, PRINT_FUNC printFunc) {
-    PAdptArray adptArray = (PAdptArray) malloc(sizeof(struct AdptArray_));
+    PAdptArray adptArray = (PAdptArray) malloc(sizeof(struct AdptArray_)); //create
     if (adptArray == NULL) {
         return NULL;
     }
-    adptArray->data = (PElement) malloc(init * sizeof(PElement));
+    adptArray->data = (PElement) malloc(init * sizeof(PElement)); //create data
     if (adptArray->data == NULL) {
         free(adptArray);
         return NULL;
     }
-    adptArray->size = 0;
-    adptArray->cap = init;
+//    adptArray->size = 0; //start from 0
+    adptArray->size = init; //no need
     adptArray->data[0] = NULL;
     adptArray->copyFunc = copyFunc;
     adptArray->delFunc = delFunc;
@@ -36,58 +36,60 @@ PAdptArray CreateAdptArray(COPY_FUNC copyFunc, DEL_FUNC delFunc, PRINT_FUNC prin
 void DeleteAdptArray(PAdptArray adptArray) {
     if (adptArray == NULL)
         return;
+    //delete data
     for (int i = 0; i < adptArray->size; ++i) {
         if (adptArray->data[i] == NULL) {
             continue;
         }
         adptArray->delFunc(adptArray->data[i]);
     }
-    free(adptArray->data);
-    free(adptArray);
+    free(adptArray->data); //free data
+    free(adptArray); //free array
 }
 
 Result SetAdptArrayAt(PAdptArray adptArray, int i, PElement element) {
     if (i < 0) {
         return FAIL;
     }
-    if (i > adptArray->cap) {
+    //realloc the array (increase it)
+    if (i > adptArray->size) {
         PElement* newData = (PElement*) realloc(adptArray->data, (i+1) * (sizeof (PElement)));
         if (newData == NULL) {
             return FAIL;
-        }
-        for (int j = adptArray->cap; j < i+1; ++j) {
+        } //make all new to NULL
+        for (int j = adptArray->size; j < i + 1; ++j) {
             newData[j] = NULL;
         }
-        adptArray->data = newData;
-        adptArray->cap = (i+1);
+        adptArray->data = newData; //update it
+        adptArray->size = (i + 1); //update the size
     }
-    if (adptArray->data[i] != NULL) {
+    if (adptArray->data[i] != NULL) { //if not empty - delete it
         adptArray->delFunc(adptArray->data[i]);
     }
     adptArray->data[i] = adptArray->copyFunc(element);
-    adptArray->size = adptArray->cap;
+//    adptArray->size = adptArray->size;
     return SUCCESS;
 }
 
 PElement GetAdptArrayAt(PAdptArray adptArray, int i) {
-    if (i < 0 || i > adptArray->cap || adptArray->data[i] == NULL) {
+    if (i < 0 || i > adptArray->size || adptArray->data[i] == NULL) {
         return NULL;
     }
-    return adptArray->copyFunc(adptArray->data[i]);
+    return adptArray->copyFunc(adptArray->data[i]); //return the data
 }
 
 int GetAdptArraySize(PAdptArray adptArray) {
     if (adptArray == NULL) {
         return -1;
     }
-    return adptArray->size;
+    return adptArray->size; //return the size
 }
 
 void PrintDB(PAdptArray adptArray) {
-    for (int i = 0; i < adptArray->cap; ++i) {
+    for (int i = 0; i < adptArray->size; ++i) {
         if (adptArray->data[i] == NULL) {
             continue;
         }
-        adptArray->printFunc(adptArray->data[i]);
+        adptArray->printFunc(adptArray->data[i]); //print
     }
 }
